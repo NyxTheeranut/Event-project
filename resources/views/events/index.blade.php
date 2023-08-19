e16e544756b06173fff9b845191e9ce8ffb24ceb
Nyx <theeranut.s@ku.th>
2023-08-19 21:00:53 +0700

    pop up

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -12,10 +12,26 @@
                     </h2>
 
                     <!-- Event Create (Auth coming soon) -->
-                    <a class="inline-block text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none"
-                       href="{{ route('events.create') }}">
+                    <a id="popupButton" class="inline-block text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none no-select">
                         สร้างกิจกรรม
                     </a>
+
+                    <!-- Popup Modal (Hidden by default) -->
+                    <div id="popupModal" class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center hidden">
+                        <div class="bg-white rounded-lg p-8">
+                            <div class="container mx-auto px-40">
+                                <h2 class="mt-12 mb-12 text-5xl font-bold leading-tight tracking-tight md:text-6xl xl:text-5xl text-gray-800">
+                                    Create Event Page
+
+
+                                    <!--ใส่เนื้อหา Create Events-->
+
+
+                                </h2>
+                            </div>
+                            <button id="closePopupButton" class="mt-4 bg-gray-300 hover:bg-gray-400 px-3 py-2 rounded-lg">Close</button>
+                        </div>
+                    </div>
 
                 </div>
             </div>
@@ -27,7 +43,7 @@
         <ul class="divide-y divide-gray-200">
 
             {{-- No Event --}}
-            @if (count($events) === 0)
+           @if (count($events) === 0)
                 <div class="flex items-center justify-center py-4 px-6">
                     <img class="w-1/6" src="https://static.vecteezy.com/system/resources/previews/003/067/848/original/cartoon-sad-smile-face-emoticon-icon-in-flat-style-free-vector.jpg" alt="Sad face">
                     <p class="text-3xl font-medium text-gray-800">404 Event Not Found</p>
@@ -47,4 +63,17 @@
 
         </ul>
     </div>
+    <script>
+        const popupButton = document.getElementById('popupButton');
+        const popupModal = document.getElementById('popupModal');
+        const closePopupButton = document.getElementById('closePopupButton');
+    
+        popupButton.addEventListener('click', function() {
+            popupModal.classList.remove('hidden');
+        });
+    
+        closePopupButton.addEventListener('click', function() {
+            popupModal.classList.add('hidden');
+        });
+    </script>
 @endsection
