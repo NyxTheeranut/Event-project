0eda58d519440fdd29ca86dd4b5a08b1ede07b85
Nyx <theeranut.s@ku.th>
2023-08-22 13:38:51 +0700

    finaltouch

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -10,32 +10,35 @@
             <div class="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2 flex">
                 <!-- (left side) -->
                 <div class="w-1/2 pr-6">
-                    <div class="typingTextEvent2 mb-2">
+                    <div class="typingTextEvent2 mb-2"  style="color: black;">
                         Event Edit.
                     </div>
                     <div class="mb-2">
-                        <label for="title" class="block mb-2 font-bold text-blue-600">ชื่อกิจกรรม:</label>
+                        <label for="title" class="block mb-2 font-bold text-blue-600"  style="color: black;">ชื่อกิจกรรม:</label>
                         @error('title') <div class="text-red-600 text-sm">{{$message}}</div> @enderror
                         <input type="text" id="name" name="title" autocomplete="off"
+                        style="color: black;"
                             value="{{ old('title', $event->title) }}"
                             placeholder="ชื่อกิจกรรม" class="border border-gray-300 @error('title') border-red-400 @enderror shadow p-3 w-full rounded mb-2" style="height: 40px; width: 100%;">
                     </div>
                     <div class="mb-2">
-                        <label for="description" class="block mb-2 font-bold text-blue-600">คำอธิบายกิจกรรม:</label>
-                        @error('description') <div class="text-red-600 text-sm">{{$message}}</div> @enderror
+                        <label for="description" class="block mb-2 font-bold text-blue-600"  style="color: black;">คำอธิบายกิจกรรม:</label>
+                        @error('description') <div class="text-red-600 text-sm" >{{$message}}</div> @enderror
                         <input id="description" name="description" autocomplete="off"
+                        style="color: black;"
                                 placeholder="คำอธิบายกิจกรรม"
                                    value="{{ old('description', $event->description) }}"
 
                                   class="border border-gray-300 @error('description') border-red-400 @enderror shadow p-3 w-full rounded mb-2" style="height: 120px; width: 100%;">{{ old('description','') }}</input>
                     </div>
                     <div class="mt-4">
-                        <label  class="block font-medium text-sm text-gray-700 dark:text-black-300 left-align-label" for="birthdate" :value="__('วันเกิด')">
+                        <label  class="block font-bold text-blue-600 dark:text-black-300 left-align-label"  style="color: black;" for="birthdate" :value="__('วันเกิด')">
                             วันเริ่มกิจกรรม
                         </label>
                         <input  class="border-gray-300 dark:border-gray-700 rounded-md shadow-sm block mt-1 w-full text-black"
                                 id="name"
                                 type="date"
+                                style="color: black;"
                                 name="start_date_time" :value="old('start_date_time')"
                                 value="{{ $event->start_date_time }}"
                                 autofocus
@@ -44,12 +47,13 @@
                     </div>
 
                     <div class="mt-4">
-                        <label for="end_date_time" class="block font-bold text-blue-600 dark:text-black-300 left-align-label" for="start_date_time" :value="__('วันเริ่มกิจกรรม')">
+                        <label for="end_date_time" class="block font-bold text-blue-600 dark:text-black-300 left-align-label"  style="color: black;" for="start_date_time" :value="__('วันเริ่มกิจกรรม')">
                             วันจบกิจกรรม:
                         </label>
                         <input class="border-gray-300 dark:border-gray-700 rounded-md shadow-sm block mt-1 w-full text-black"
                                id="end_date_time"
                                type="date"
+                               style="color: black;"
                                name="end_date_time"
                                value="{{ old('end_date_time', $event->end_date_time) }}"
                                required
@@ -61,16 +65,17 @@
                 <!-- (right side) -->
                 <div class="w-1/2 pl-6 border-l">
                     <div class="mb-2" style = "margin-top:3.6rem;">
-                        <label for="title" class="block mb-2 font-bold text-blue-600">งบจัดกิจกรรม:</label>
+                        <label for="title" class="block mb-2 font-bold text-blue-600" style="color: black;">งบจัดกิจกรรม:</label>
                         @error('budget') <div class="text-red-600 text-sm">{{ $message }}</div> @enderror
                         <input type="text" id="name" name="budget" autocomplete="off"
+                        style="color: black;"
                             value="{{ old('budget', $event->budget)}}"
                             placeholder="งบจัดกิจกรรมใหม่..."
                                class="border border-gray-300 @error('textbox1') border-red-400 @enderror shadow p-3 w-full rounded mb-2"
                                style="height: 40px; width: 100%;" />
                     </div>
                     <div class="mb-3">
-                        <label for="name" class="block font-bold text-blue-600">รายชื่อผู้จัดทำกิจกรรม:</label>
+                        <label for="name" class="block font-bold text-blue-600" style="color: black;">รายชื่อผู้จัดทำกิจกรรม:</label>
                     </div>
                     <!-- loop ($staffs as $staff)
 
